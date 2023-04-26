97109c16c150a3b54dd1c67bc7be55e1d4df73dc
Guentalina Allegretti <guentalina.allegretti@gmail.com>
2023-04-26 22:40:48 +0200

    [EDIT]cookie

--- resources/views/vendor/cookie-consent/dialogContents.blade.php
+++ resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -1,12 +1,12 @@
 <div class="js-cookie-consent cookie-consent fixed bottom-0 inset-x-0 pb-2">
-    <div class="max-w-7xl mx-auto px-6 fixed-bottom cg-bg-primary p-2 text-light text-center">
+    <div class="max-w-7xl mx-auto px-6 fixed-bottom section-bg p-2 text-light text-center">
         <div class="p-2 rounded-lg">
            
             <div class="flex items-center justify-between flex-wrap">
                 <div class="row">
                     <div class="col-12">
                         <div class="w-0 flex-1 items-center hidden md:inline">
-                            <p class="ml-3 text-white fw-lighter fs-6 cookie-consent__message">
+                            <p class="ml-3 text-black fw-light fs-6 cookie-consent__message">
                                 {!! trans('cookie-consent::texts.message') !!}
                             </p>
                         </div>   
@@ -16,10 +16,10 @@
                  <div class="row">
                     <div class="col-12">
                         <div class="mt-2 flex-shrink-0 w-full sm:mt-0 sm:w-auto">
-                            <button class="masthead-btn js-cookie-consent-agree cookie-consent__agree flex items-center justify-center px-4 py-2 rounded-md text-sm fw-lighter fs-6 me-3">
-                                <a href="{{route('cookies')}}" class="link-cg">Leggi di più</a>
+                            <button class="btn-masthead js-cookie-consent-agree cookie-consent__agree flex items-center justify-center px-4 py-2 rounded-md text-sm fw-light fs-6 me-3">
+                                <a href="{{route('cookies')}}">Leggi di più</a>
                             </button>
-                            <button class="masthead-btn js-cookie-consent-agree cookie-consent__agree flex items-center justify-center px-4 py-2 rounded-md text-sm fw-lighter fs-6 me-3">
+                            <button class="btn-masthead js-cookie-consent-agree cookie-consent__agree flex items-center justify-center px-4 py-2 rounded-md text-sm fw-light fs-6 me-3">
                                 {{ trans('cookie-consent::texts.agree') }}
                             </button>
                         </div>
