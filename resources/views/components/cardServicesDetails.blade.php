028088c1b6f618d364c476b4da25a28152c4787d
Guentalina Allegretti <guentalina.allegretti@gmail.com>
2023-05-27 19:52:32 +0200

    change service

---
+++ resources/views/components/cardServicesDetails.blade.php
@@ -0,0 +1,29 @@
+<div class="col-lg-5 d-flex flex-column justify-content-center">
+  <h3 class="fs-3">{{$name}}</h3>
+
+  <div class="icon d-flex position-relative" data-aos="fade-up" data-aos-delay="100">
+    <i class="bi bi-caret-right p-3"></i>
+    <div>
+      <p>{{$description1}}</p>
+    </div>
+  </div>
+
+  <div class="icon d-flex position-relative" data-aos="fade-up" data-aos-delay="200">
+    <i class="bi bi-caret-right p-3"></i>
+    <div>
+      <p>{{$description2}}</p>
+    </div>
+  </div>
+
+  <div class="icon d-flex position-relative" data-aos="fade-up" data-aos-delay="400">
+    <i class="bi bi-caret-right p-3"></i>
+    <div>
+      <p>{{$description3}}</p>
+    </div>
+  </div>
+</div>
+
+<div class="col-lg-6" data-aos="zoom-in" data-aos-delay="100">
+  <img src="{{$img}}" class="img-fluid" alt="progetto">
+</div>
+</div>
